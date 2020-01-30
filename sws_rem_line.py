706c25ab572d622e7e3469dc0006f452af15f919
Jaime Bruno Cirne de Oliveira <jaime@neuro.ufrn.br>
2020-01-30 13:14:35 -0300

    SWS REM fig

---
+++ sws_rem_line.py
@@ -0,0 +1,49 @@
+#! /usr/bin/env python
+"""
+[find the interval and duration about SWS and REM related and create a img]
+
+"""
+# Authors: Jaime Bruno Cirne de Oliveira (jaime@neuro.ufrn.br)
+
+from __future__ import division
+
+from collections import Counter
+import sys
+import csv
+import statistics
+import numpy as np
+import sys
+import os
+import matplotlib.pyplot as plt
+import uOSBlib as ulib
+from scipy import stats
+
+
+
+if __name__ == '__main__':
+
+    sizefone = 18
+
+    ulib.create_data_from_raw()
+    sws_durations = []
+    rem_durations = []
+    with open(ulib.path_data['processed']+'sws_before_rem_duration.csv', 'r') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=';', quotechar='|')
+        for row in spamreader:
+            if row[0] != 'SWS-like':
+                sws_durations.append(int(row[0]))
+                rem_durations.append(int(row[1]))
+
+
+    coef = np.polyfit(sws_durations,rem_durations,1)
+    poly1d_fn = np.poly1d(coef) 
+
+    plt.suptitle("Line Regress SWS-like and REM-like related",fontname='Arial', size=sizefone+2, weight="bold")
+    plt.plot(sws_durations,rem_durations, 'ko', sws_durations, poly1d_fn(sws_durations), '--r')
+    plt.ylabel("Duration of REM-like (seconds)", fontname='Arial',size=sizefone, weight="bold")    
+    plt.xlabel("Duration of SWS-like (seconds)", fontname='Arial',size=sizefone, weight="bold")
+    plt.xticks( fontname='Arial', size=sizefone-2, weight="bold")
+    plt.yticks( fontname='Arial', size=sizefone-2, weight="bold")
+
+    plt.show()
+
